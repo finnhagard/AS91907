0fe10f9bd087c008b971c5a33af6cf73dafd3431
Finn Hagard <finnhagard@outlook.com>
2026-06-19 20:14:09 +1200

    added: extreme boilerplate code for frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import Header from "./Header";
+import Homepage from "./Homepage";
+
+function App() {
+    return (
+        <>
+        <Header />
+        <Homepage />
+        </>
+    );
+}
+
+export default App;
---
+++ frontend/src/homepage.jsx
@@ -0,0 +1,9 @@
+function homepage() {
+    return (
+        <main>
+            <h1>Welcome to Vision College Learning Centre</h1>
+        </main>
+    );
+}
+
+export default homepage;
