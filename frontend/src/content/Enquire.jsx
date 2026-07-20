33a2f9552d148b2e4e933ec49c48c45394badcf1
Lucas Harford <22127@hillcrest-high.school.nz>
2026-07-20 12:14:56 +1200

    add: enquire form outline

--- frontend/src/content/Enquire.jsx
+++ frontend/src/content/Enquire.jsx
@@ -7,6 +7,18 @@ function Enquire() {
                 <h1>Enquire</h1>
                 <p>things go here :)</p>
                 <p>i don't know how this is going to function yet, or if we are even going to do this part on-site, sooo yeah</p>
+                <p><br></br></p>
+                <p>Name:</p>
+                <p>Contact email:</p>
+                <p>Nature of enquiry:</p>
+                <ul>
+                    <li>Course info</li>
+                    <li>Pricing</li>
+                    <li>Services</li>
+                    <li>Other</li>
+                </ul>
+                <p><br></br></p>
+                <p>Enquiry:</p>
             </section>
         </main>
     );
