0bd33dd827eb906dd867ef7b7d8610555d4980e6
Lucas Harford <lucash7259@gmail.com>
2026-07-21 12:39:48 +1200

    add: apply page branch

--- frontend/src/content/Apply.jsx
+++ frontend/src/content/Apply.jsx
@@ -7,6 +7,8 @@ function Apply() {
                 <h1>Apply</h1>
                 <p>things go here :)</p>
                 <p>i don't know how this is going to function yet, or if we are even going to do this part on-site, sooo yeah</p>
+                <p><br></br></p>
+                <p>daisee your turn. heres a branch. make a form.</p>
             </section>
         </main>
     );
