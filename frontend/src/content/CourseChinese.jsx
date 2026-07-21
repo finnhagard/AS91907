12a91366b4d0b5c63b4cd30175780dc04a6a7849
Lucas Harford <lucash7259@gmail.com>
2026-07-21 13:11:22 +1200

    add: individual course pages

---
+++ frontend/src/content/CourseChinese.jsx
@@ -0,0 +1,26 @@
+import "./CoursePages.css";
+
+function CourseChinese() {
+    return (
+        <main className="ChineseCourse">
+            <section className="lang_course_content">
+                <h1>Learn Chinese</h1>
+                <p>Course info stuff goes here</p>
+                <p>We're so wonderful!</p>
+                <p>You should learn a language with us! Or two!</p>
+                <div className="lang_course_grid">
+                    <div className="lang_course_flyer">
+                        <h2>See this amazing flyer of people learning!</h2>
+                        <p>image goes here and lorem ipsum</p>
+                    </div>
+                    <div className="lang_course_flyer">
+                        <h2>See another amazing flying of people talking!</h2>
+                        <p>image goes here and lorem ipsum</p>
+                    </div>
+                </div>
+            </section>
+        </main>
+    );
+}
+
+export default CourseChinese;
---
+++ frontend/src/content/CourseEnglish.jsx
@@ -0,0 +1,24 @@
+import "./CoursePages.css";
+
+function CourseEnglish() {
+    return (
+        <main className="EnglishCourse">
+            <section className="lang_course_content">
+                <h1>Learn English</h1>
+                <p>Course info stuff goes here</p>
+                <div className="lang_course_grid">
+                    <div className="lang_course_flyer">
+                        <h2>See this amazing flyer of people learning!</h2>
+                        <p>image goes here and lorem ipsum</p>
+                    </div>
+                    <div className="lang_course_flyer">
+                        <h2>See another amazing flying of people talking!</h2>
+                        <p>image goes here and lorem ipsum</p>
+                    </div>
+                </div>
+            </section>
+        </main>
+    );
+}
+
+export default CourseEnglish;
