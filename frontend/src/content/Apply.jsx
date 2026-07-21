997463dc8d34a56b95e7a6f8f3d18151e5f268ca
Daisy Evans <22497@hillcrest-high.school.nz>
2026-07-21 13:29:40 +1200

    add: application form in progress

--- frontend/src/content/Apply.jsx
+++ frontend/src/content/Apply.jsx
@@ -4,11 +4,106 @@ function Apply() {
     return (
         <main className="apply">
             <section className="apply_content">
-                <h1>Apply</h1>
-                <p>things go here :)</p>
-                <p>i don't know how this is going to function yet, or if we are even going to do this part on-site, sooo yeah</p>
-                <p><br></br></p>
-                <p>daisee your turn. heres a branch. make a form.</p>
+                <form>
+                    <table align="center" width="700" cellPadding="10">
+                        <thead>
+                            <tr>
+                                <td colSpan="2" align="center">
+                                    <h1>Student Application Form</h1>
+                                </td>
+                        </tr>
+                        </thead>
+                        <tbody>
+                            {/*name input*/}
+                            <tr>
+                                <td>
+                                    <label htmlFor="fname">First name:</label>
+                                </td>
+                                <td>
+                                    <label htmlFor="lname">Last name:</label>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>
+                                    <input type="text" size="21" placeholder="First Name" id="fname"  name="fname"/>
+                                </td>
+                                <td>
+                                    <input type="text" size="21" placeholder="Last Name" id="lname"  name="lname"/>
+                                </td>
+                            </tr>
+
+                            {/*NSN input*/}
+                            <tr>
+                                <td>
+                                    <label htmlFor="nsn">NSN (if known):</label>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>
+                                    <input type="number" size="21" placeholder="NSN" id="nsn"  name="nsn"/>
+                                </td>
+                            </tr>
+
+                            {/*email input*/}
+                            <tr>
+                                <td><label htmlFor="email">Email:</label></td>
+                            </tr>
+                            <tr>
+                                <td><input type="email" placeholder="Email" id="email" name="email"/></td>
+                            </tr>
+
+                            {/*phone input*/}
+                            <tr>
+                                <td>Phone number:</td>
+                                <td>
+                                    <input type="tel" size="50" placeholder="Phone"/>
+                                </td>
+                            </tr>
+
+                            {/*DOB input*/}
+                            <tr>
+                                <td>Date of birth:</td>
+                                <td>
+                                    <input type="date" id="birthday"/>
+                                </td>
+                            </tr>
+
+                            {/*gender input*/}
+                            <tr>
+                                <td>Gender :</td>
+                                <td>
+                                    <input type="radio" name="g"/> Male
+                                    <input type="radio" name="g"/> Female
+                                    <input type="radio" name="g"/> Other
+                                </td>
+                            </tr>
+
+                            {/*course input*/}
+                            <tr>
+                                <td>Course :</td>
+                                <td><select>
+                                    <option>Select</option>
+                                    <option>Chinese</option>
+                                    <option>English</option>
+                                </select>
+                                </td>
+                            </tr>
+
+                            {/*address input*/}
+                            <tr>
+                                <td>Address :</td>
+                                <td><textarea cols="48" rows="5"></textarea></td>
+                            </tr>
+
+
+                            <tr>
+                                <td colSpan="2" align="center">
+                                    <input type="submit"/>
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </form>
             </section>
         </main>
     );
