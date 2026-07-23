bb8c734ef6a4f8476da8bf7e51105d2af47f163f
Daisy Evans <daisyeevans@gmail.com>
2026-07-23 16:59:29 +1200

    add new wip form

--- frontend/src/content/Apply.jsx
+++ frontend/src/content/Apply.jsx
@@ -4,178 +4,62 @@ function Apply() {
     return (
         <main className="apply">
             <section className="apply_content">
-                <form>
-                    <table align="center" width="700" cellPadding="10">
-                        <thead>
-                            <tr>
-                                <td colSpan="2" align="center">
-                                    <h1>Student Application Form</h1>
-                                </td>
-                        </tr>
-                        </thead>
-                        <tbody>
-                            {/*name input*/}
-                            <tr>
-                                <td>
-                                    <label htmlFor="fname">First name:</label>
-                                </td>
-                                <td>
-                                    <label htmlFor="lname">Last name:</label>
-                                </td>
-                            </tr>
-                            <tr>
-                                <td>
-                                    <input type="text" size="21" placeholder="First Name" id="fname"  name="fname"/>
-                                </td>
-                                <td>
-                                    <input type="text" size="21" placeholder="Last Name" id="lname"  name="lname"/>
-                                </td>
-                            </tr>
-
-                            {/*NSN input*/}
-                            <tr>
-                                <td>
-                                    <label htmlFor="nsn">NSN (if known):</label>
-                                </td>
-                            </tr>
-                            <tr>
-                                <td>
-                                    <input type="number" size="21" placeholder="NSN" id="nsn"  name="nsn"/>
-                                </td>
-                            </tr>
-
-                            {/*email input*/}
-                            <tr>
-                                <td><label htmlFor="email">Email:</label></td>
-                            </tr>
-                            <tr>
-                                <td><input type="email" placeholder="Email" id="email" name="email"/></td>
-                            </tr>
-
-                            {/*DOB input*/}
-                            <tr>
-                                <td>
-                                    <label htmlFor="birthday">Date of Birth:</label>
-                                </td>
-                            </tr>
-                            <tr>
-                                <td>
-                                    <input type="date" id="birthday" name="birthday"/>
-                                </td>
-                            </tr>
-
-                            {/*gender input*/}
-                            <tr>
-                                <td>
-                                    <label htmlFor="gender">Gender:</label>
-                                </td>
-                            </tr>
-                            <tr>
-                                <td colSpan="3">
-                                    <input type="radio" name="gender" id="female"/>
-                                    <label htmlFor="female">Female</label>
-                                    <input type="radio" name="gender" id="male"/>
-                                    <label htmlFor="male">Male</label>
-                                    <input type="radio" name="gender" id="other"/>
-                                    <label htmlFor="other">Other</label>
-                                </td>
-                                <td>
-
-                                </td>
-                                <td>
-
-                                </td>
-                            </tr>
-
-                            {/*current high school*/}
-
-                            {/*year level*/}
-
-                            <tr>
-                                <td>
-                                    <label htmlFor="school">Current high school:</label>
-                                </td>
-                                <td>
-                                    <label htmlFor="year">Current year level:</label>
-                                </td>
-                            </tr>
-                            <tr>
-                                <td>
-                                    <input type="text" name="school" id="school"/>
-                                </td>
-                                <td>
-                                    <select id="year" name="year">
-                                        <option value="">--- Select ---</option>
-                                        <option value="ynine">Year 9</option>
-                                        <option value="yten">Year 10</option>
-                                        <option value="yeleven">Year 11</option>
-                                        <option value="ytwelve">Year 12</option>
-                                        <option value="ythirteen">Year 13</option>
-                                    </select>
-                                </td>
-                            </tr>
-
-                            {/*course input*/}
-                            <tr>
-                                <td>
-                                    <label htmlFor="course">Course:</label>
-                                </td>
-                            </tr>
-                            <tr>
-                                <td>
-                                    <select id="course" name="course">
-                                        <option value="">--- Select ---</option>
-                                        <option value="chinese">Chinese</option>
-                                        <option value="english">English</option>
-                                    </select>
-                                </td>
-                            </tr>
-
-                            {/*preferred time*/}
-                            <tr>
-                                <td>
-                                    <label htmlFor="time">Preferred time:</label>
-                                </td>
-                            </tr>
-                            <tr>
-                                <td>
-                                    <select id="time" name="time">
-                                        <option value="">--- Select ---</option>
-                                        <option value="monafternoon">Monday afternoons</option>
-                                        <option value="wedsafternoon">Wednesday afternoons</option>
-                                        <option value="satmorning">Saturday mornings</option>
-                                    </select>
-                                </td>
-                            </tr>
-
-                            {/*phone input*/}
-                            <tr>
-                                <td><label htmlFor= "phone">Phone Number:</label> </td>
-                            </tr>
-                            <tr>
-                                <td>
-                                    <input type="tel" size="20" placeholder="Phone" id="phone" name="phone"/>
-                                </td>
-                            </tr>
-
-                            {/*address input*/}
-                            <tr>
-                                <td>Address :</td>
-                                <td><textarea cols="48" rows="5"></textarea></td>
-                            </tr>
-
-                            {/*emergency contact*/}
-
-                            {/*next of kin*/}
-
-                            <tr>
-                                <td colSpan="2" align="center">
-                                    <input type="submit"/>
-                                </td>
-                            </tr>
-                        </tbody>
-                    </table>
-                </form>
+                <h1>Apply</h1>
+                <div className="form">
+                    <form>
+                        <h2>Personal Details</h2>
+                        <div className="multi-column">
+                            <label htmlFor="givenname">Given name(s)*</label><br></br>
+                            <input type="text" id="givenname" name="givenname" size="27" required/><br></br><br></br>
+                            <label htmlFor="prefname">Preferred name</label><br></br>
+                            <input type="text" id="prefname" name="prefname" size="27"/>
+                        </div>
+                        <div className="multi-column">
+                            <label htmlFor="surname">Surname*</label><br></br>
+                            <input type="text" id="surname" name="surname" size="27" required/><br></br><br></br>
+                            <label htmlFor="nsn">NSN (if known)</label><br></br>
+                            <input type="text" id="nsn" name="nsn" size="27" /><br></br>
+                        </div><br></br>
+                        <label htmlFor="email">Email*</label><br></br>
+                        <input type="email" id="email" name="email" size="27" required/><br></br><br></br>
+                        <div className="multi-column">
+                            <label htmlFor="birthday">Date of birth*</label><br></br>
+                            <input type="date" id="birthday" name="birthday" className="dategenderyear" required/>
+                        </div>
+                        <div className="multi-column">
+                            <label htmlFor="gender">Gender*</label><br></br>
+                            <select id="gender" name="gender" className="dategenderyear" required>
+                                <option value="">--- Select ---</option>
+                                <option value="male">Male</option>
+                                <option value="female">Female</option>
+                                <option value="other">Other</option>
+                            </select>
+                        </div><br></br>
+                        <div className="multi-column">
+                          <label htmlFor="school">Current high school*</label><br></br>
+                          <input type="text" id="school" name="school" size="27" required/>
+                        </div>
+                        <div className="multi-column">
+                            <label htmlFor="year">Year level*</label><br></br>
+                            <select id="year" name="year" className="dategenderyear" required>
+                                <option value="">--- Select ---</option>
+                                <option value="year9">Year 9</option>
+                                <option value="year10">Year 10</option>
+                                <option value="year11">Year 11</option>
+                                <option value="year12">Year 12</option>
+                                <option value="year13">Year 13</option>
+                            </select>
+                        </div><br></br><br></br>
+                        <h2>Course Details</h2>
+                        <div>
+                            <label htmlFor="course">Course*</label><br></br>
+                            <input type="radio" id="english" name="course" value="English"/>
+                            <label htmlFor="english">English</label>
+                            <input type="radio" id="chinese" name="course" value="English"/>
+                            <label htmlFor="chinese">Chinese</label>
+                        </div>
+                    </form>
+                </div>
             </section>
         </main>
     );
