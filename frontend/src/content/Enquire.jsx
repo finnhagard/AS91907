f6d1ae49e0b97a3e68b36eb5d7488afada11a9ad
Daisy Evans <daisyeevans@gmail.com>
2026-07-31 09:49:32 +1200

    add: red asterisk. change radios to checkbox (wip)

--- frontend/src/content/Enquire.jsx
+++ frontend/src/content/Enquire.jsx
@@ -36,28 +36,29 @@ function Enquire() {
                 <h1>Enquire</h1>
                 <div className="form">
                     <form onSubmit={handleSubmit}>
+                        <p className="subheading"><span className="asterisk">*</span> indicates a required question</p>
                         <div className="multi-column">
-                            <label htmlFor="egivenname">Given name(s)*</label><br />
+                            <label htmlFor="egivenname">Given name(s)<span className="asterisk">*</span></label><br />
                             <input type="text" id="egivenname" name="egivenname" size="35" required />
                         </div>
 
                         <div className="multi-column">
-                            <label htmlFor="esurname">Surname*</label><br />
+                            <label htmlFor="esurname">Surname<span className="asterisk">*</span></label><br />
                             <input type="text" id="esurname" name="esurname" size="35" required />
                         </div><br/>
 
                         <div>
-                            <label htmlFor="ephone">Phone number*</label><br />
+                            <label htmlFor="ephone">Phone number<span className="asterisk">*</span></label><br />
                             <input type="tel" id="e" name="ephone" size="54" required />
                         </div><br/>
 
                         <div className="multi-column">
-                            <label htmlFor="school">Current high school*</label><br />
+                            <label htmlFor="school">Current high school<span className="asterisk">*</span></label><br />
                             <input type="text" id="school" name="school" size="35" required />
                         </div>
 
                         <div className="multi-column">
-                            <label htmlFor="eyear">Year level*</label><br />
+                            <label htmlFor="eyear">Year level<span className="asterisk">*</span></label><br />
                             <select id="eyear" name="eyear" className="dropdown" required>
                                 <option value="">--- Select ---</option>
                                 <option value="year9">Year 9</option>
@@ -69,17 +70,17 @@ function Enquire() {
                         </div><br/>
 
                         <div>
-                            <label htmlFor="ecourse" required>Course of interest*</label><br />
+                            <label htmlFor="ecourse" required>Course(s) of interest<span className="asterisk">*</span></label><br />
                             <div className="coursesdiv">
-                                <input type="radio" id="e-english" name="ecourse" value="English" />
+                                <input type="checkbox" id="e-english" name="ecourse" value="English" />
                                 <label htmlFor="e-english" className="radio-label">English</label>
-                                <input type="radio" id="echinese" name="ecourse" value="Chinese" />
+                                <input type="checkbox" id="echinese" name="ecourse" value="Chinese" />
                                 <label htmlFor="echinese" className="radio-label">Chinese</label>
                             </div>
                         </div><br/>
 
                         <div>
-                            <label htmlFor="etime">Preferred teaching time*</label><br />
+                            <label htmlFor="etime">Preferred teaching time<span className="asterisk">*</span></label><br />
                             <select id="etime" name="etime" className="dropdown" required>
                                 <option value="">--- Select ---</option>
                                 <option value="option1">Tuesday afternoon</option>
@@ -89,19 +90,19 @@ function Enquire() {
                         </div><br/>
 
                         <div>
-                            <label htmlFor="enqnature">Nature of enquiry*</label><br />
+                            <label htmlFor="enqnature">Nature of enquiry<span className="asterisk">*</span></label><br />
 
-                            <input type="radio" id="courseinfo" name="enqnature" value="Course Information" checked={enqNature === "Course Information"} onChange={handleRadioChange} />
-                            <label htmlFor="courseinfo" className="radio-label">Course Information</label><br />
+                            <input type="checkbox" id="courseinfo" name="enqnature" value="Course Information" checked={enqNature === "Course Information"} onChange={handleRadioChange} />
+                            <label htmlFor="courseinfo" className="checkbox-label">Course Information</label><br />
 
-                            <input type="radio" id="pricing" name="enqnature" value="Pricing" checked={enqNature === "Pricing"} onChange={handleRadioChange} />
-                            <label htmlFor="pricing" className="radio-label">Pricing</label><br />
+                            <input type="checkbox" id="pricing" name="enqnature" value="Pricing" checked={enqNature === "Pricing"} onChange={handleRadioChange} />
+                            <label htmlFor="pricing" className="checkbox-label">Pricing</label><br />
 
-                            <input type="radio" id="services" name="enqnature" value="Services" checked={enqNature === "Services"} onChange={handleRadioChange} />
-                            <label htmlFor="services" className="radio-label">Services</label><br />
+                            <input type="checkbox" id="services" name="enqnature" value="Services" checked={enqNature === "Services"} onChange={handleRadioChange} />
+                            <label htmlFor="services" className="checkbox-label">Services</label><br />
 
-                            <input type="radio" id="other" name="enqnature" value="Other" checked={enqNature === "Other"} onChange={handleRadioChange} />
-                            <label htmlFor="other" className="radio-label">Other</label><br /><br />
+                            <input type="checkbox" id="other" name="enqnature" value="Other" checked={enqNature === "Other"} onChange={handleRadioChange} />
+                            <label htmlFor="other" className="checkbox-label">Other</label><br /><br />
 
                             {enqNature === "Other" && (
                                 <div id="otherInputContainer">
@@ -118,7 +119,7 @@ function Enquire() {
                             )}
                         </div><br/>
                         <div className="enquirybox">
-                            <label htmlFor="enquiry" required>Enquiry:</label><br/>
+                            <label htmlFor="enquiry" required>Enquiry<span className="asterisk">*</span></label><br/>
                             <textarea id="enquiry" name="enquiry" rows="8" cols="60"></textarea>
                         </div>
 
