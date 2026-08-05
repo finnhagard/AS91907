5dc5fd703680c57733995571b2acb31ea166210e
Finn Hagard <finnhagard@outlook.com>
2026-08-05 18:23:14 +1200

    add: about us styling and img (placeholder)

--- frontend/src/content/AboutUs.jsx
+++ frontend/src/content/AboutUs.jsx
@@ -5,12 +5,16 @@ function AboutUs() {
     return (
         <main className="about">
             <title>Vision College Learning Centre - About Us</title>
-            <section className="about_content">
-                <h1>About Us</h1>
-                <p>i was here</p>
-                <p>and uhhh</p>
-                <p>lorem ipsum</p>
-            </section>
+            <div className="about_layout">
+                <section className="about_content">
+                    <h1>About Us</h1>
+                    <p>i was here</p>
+                    <p>and uhhh</p>
+                    <p>lorem ipsum</p>
+                </section>
+
+                <img src="/about-img.jpg" alt="" className="about_img" />
+            </div>
         </main>
     );
 }
