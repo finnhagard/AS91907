55ba519fe9dd3c3ec27726049c7f7ddf1705cefc
Finn Hagard <finnhagard@outlook.com>
2026-08-07 09:46:57 +1200

    added aboutus text

--- frontend/src/content/AboutUs.jsx
+++ frontend/src/content/AboutUs.jsx
@@ -8,9 +8,10 @@ function AboutUs() {
             <div className="about_layout">
                 <section className="about_content">
                     <h1>About Us</h1>
-                    <p>i was here</p>
-                    <p>and uhhh</p>
-                    <p>lorem ipsum</p>
+                    <p>Vision College is an education provider established over 40 years ago where we have been championing student success ever since. We have 3 campuses across New Zealand, Hamilton being Head Office, Auckland and Christchurch.
+                        Vision College offers Tertiary Education through to a degree in Counselling, a well-established School of Information Technology, Youth programmes, ESOL and Eary Childhood to name a few.
+                        The learning Centre is an initiative of Vision College to assist secondary school students on their learning pathways initially in English and Chinese with more programmes to be added over time.
+                    </p>
                 </section>
 
                 <img src="/about-img.jpg" alt="" className="about_img" />
