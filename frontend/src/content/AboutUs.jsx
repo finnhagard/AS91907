4a8da449c36687a40a2b4c957b8693ceb01f1644
Finn Hagard <finnhagard@outlook.com>
2026-08-07 14:32:19 +1200

    fixed: spelling mistake in the provided aboutus information

--- frontend/src/content/AboutUs.jsx
+++ frontend/src/content/AboutUs.jsx
@@ -9,7 +9,7 @@ function AboutUs() {
                 <section className="about_content">
                     <h1>About Us</h1>
                     <p>Vision College is an education provider established over 40 years ago where we have been championing student success ever since. We have 3 campuses across New Zealand, Hamilton being Head Office, Auckland and Christchurch.
-                        Vision College offers Tertiary Education through to a degree in Counselling, a well-established School of Information Technology, Youth programmes, ESOL and Eary Childhood to name a few.
+                        Vision College offers Tertiary Education through to a degree in Counselling, a well-established School of Information Technology, Youth programmes, ESOL and Early Childhood to name a few.
                         The learning Centre is an initiative of Vision College to assist secondary school students on their learning pathways initially in English and Chinese with more programmes to be added over time.
                     </p>
                 </section>
